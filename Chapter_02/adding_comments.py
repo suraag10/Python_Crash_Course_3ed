64a6ced7383a78634eb0377225d6ee4bf0e41207
Suraag Baniya <suraag.baniya.sb@gmail.com>
2025-07-22 14:02:17 +0545

    chapter 2 completed

---
+++ Chapter_02/adding_comments.py
@@ -0,0 +1,15 @@
+''' 2-9 '''
+
+# this is a program which prints out my favourite number
+
+fav_number = 10
+print(f"{fav_number} is my favourite number.")
+
+# this is another program that calculates number 8 using addition,subtraction,multiplication,division
+
+'''2-8'''
+
+print(5+3)
+print(10-2)
+print(4*2)
+print(16/2)
